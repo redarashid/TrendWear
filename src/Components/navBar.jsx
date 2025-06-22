a81f8d295094ef8c4f9749bf5b12db54e34d3fab
redarashid <redarashid824@gmail.com>
2025-06-22 15:00:38 +0300

    first commit

---
+++ src/Components/navBar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const navBar = () => {
+  return <header>
+    <span className="">
+        Use promo code 
+        <span>
+            <a href="https://www.linkedin.com/in/rashid-reda-4048a5314/" target="_blank">Rashid</a>
+        </span>
+        <span>for 20% discount</span>
+    </span>
+  </header>;
+};
+
+export default navBar;
