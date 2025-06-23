af46b5133f72e46f3a802dd48025b3d7c2589454
redarashid <redarashid824@gmail.com>
2025-06-23 12:24:52 +0300

    first commit

--- src/Components/navBar.jsx
+++ src/Components/navBar.jsx
@@ -2,18 +2,12 @@ import React from "react";
 
 const NavBar = () => {
   return (
-    <header className=" bg-black text-white text-center p-2">
-      <span className="">
-        Use promo code
+    <header className=" bg-black font-thin text-white text-center p-2">
+        <span>Use promo code </span>
         <span>
-          <a
-            href="https://www.linkedin.com/in/rashid-reda-4048a5314/"
-            target="_blank">
-            Rashid
-          </a>
+          <a className=" font-bold" href="https://www.linkedin.com/in/rashid-reda-4048a5314/" target="_blank"> Rashid </a>
         </span>
         <span>for 20% discount</span>
-      </span>
     </header>
   );
 };
