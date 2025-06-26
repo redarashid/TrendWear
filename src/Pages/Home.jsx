b2743da217428f90276719a559e7b6f83cda5f70
redarashid <redarashid824@gmail.com>
2025-06-26 15:02:57 +0300

    first commit

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,44 +1,43 @@
-import { useEffect, useRef, memo } from 'react'
+import { useEffect, useRef, memo } from "react";
 
-import { Link } from 'react-router-dom'
-import Reavel from '../Reavel'
-import { motion } from 'framer-motion'
-import BrandsBar from '../Components/BrandsBar'
-import HomeSliders from '../Components/HomeSliders'
-import HappyCustomers from '../Components/HappyCustomers'
-import Img from '../Components/Img'
+import { Link } from "react-router-dom";
+import Reavel from "../Reavel";
+import { motion } from "framer-motion";
+import BrandsBar from "../components/BrandsBar";
+import HomeSliders from "../components/HomeSliders";
+import HappyCustomers from "../components/HappyCustomers";
+import Img from "../components/Img";
 
-export default function Home({ to = '' }) {
-  const NewArrival = useRef()
-  const TopSelling = useRef()
-  const OnSale = useRef()
+export default function Home({ to = "" }) {
+  const NewArrival = useRef();
+  const TopSelling = useRef();
+  const OnSale = useRef();
 
   useEffect(() => {
-    if (to === '') {
+    if (to === "") {
       window.scrollTo({
         top: 0,
-        behavior: 'smooth'
-      })
+        behavior: "smooth",
+      });
       return;
     }
-    if (to === 'NewArrival') {
+    if (to === "NewArrival") {
       window.scrollTo({
         top: NewArrival.current.offsetTop,
-        behavior: 'smooth'
-      })
-    } else if (to === 'TopSelling') {
+        behavior: "smooth",
+      });
+    } else if (to === "TopSelling") {
       window.scrollTo({
         top: TopSelling.current.offsetTop,
-        behavior: 'smooth'
-      })
-    } else if (to === 'OnSale') {
+        behavior: "smooth",
+      });
+    } else if (to === "OnSale") {
       window.scrollTo({
         top: OnSale.current.offsetTop,
-        behavior: 'smooth'
-      })
+        behavior: "smooth",
+      });
     }
-
-  }, [to])
+  }, [to]);
 
   return (
     <>
@@ -47,25 +46,32 @@ export default function Home({ to = '' }) {
           {/* القسم النصي */}
           <div className="nav:w-full lg:w-1/2 flex flex-col flex-grow lg:pr-10">
             <h1 className="bolded text-2xl sm:text-4xl md:text-6xl mb-5 sm:mb-10 max-w-[550px] whitespace-nowrap">
-              <Reavel>FIND CLOTHES</Reavel> <Reavel>THAT MATCHES</Reavel> <Reavel>YOUR STYLE</Reavel>
+              <Reavel>FIND CLOTHES</Reavel> <Reavel>THAT MATCHES</Reavel>
+              <Reavel>YOUR STYLE</Reavel>
             </h1>
 
             <p className="max-w-[550px] text-sm sm:text-base md:text-lg">
-             Browse through our diverse range of meticulously crafted garments, designed to bring out your individuality and cater to your sense of style.
-
+              Browse through our diverse range of meticulously crafted garments,
+              designed to bring out your individuality and cater to your sense
+              of style.
             </p>
 
-
-            <div className='mt-10'></div>
-            <Reavel className='btnReavel flex' btn='w-full lg:w-fit'>
-              <Link className="btn w-full lg:w-fit text-center hover:bg-gray-600 " to='Shop'>Shop Now</Link>
+            <div className="mt-10"></div>
+            <Reavel className="btnReavel flex" btn="w-full lg:w-fit">
+              <Link
+                className="btn w-full lg:w-fit text-center hover:bg-gray-600 "
+                to="Shop">
+                Shop Now
+              </Link>
             </Reavel>
-            <div className='mb-10'></div>
+            <div className="mb-10"></div>
             <div className="flex flex-col sm:flex-row flex-wrap justify-center items-center gap-4">
               <div className="flex justify-center items-center gap-2 w-full sm:w-auto">
                 <div className="flex flex-col justify-center items-center border-r border-gray-300 pr-3">
                   <Reavel>
-                    <h1 className="font-bold text-2xl sm:text-3xl tracking-wide">200+</h1>
+                    <h1 className="font-bold text-2xl sm:text-3xl tracking-wide">
+                      200+
+                    </h1>
                   </Reavel>
                   <Reavel>
                     <p className="whitespace-nowrap">International Brands</p>
@@ -74,7 +80,9 @@ export default function Home({ to = '' }) {
 
                 <div className="flex flex-col justify-center items-center border-r border-gray-300 pr-3">
                   <Reavel>
-                    <h1 className="font-bold text-2xl sm:text-3xl tracking-wide">2,000+</h1>
+                    <h1 className="font-bold text-2xl sm:text-3xl tracking-wide">
+                      2,000+
+                    </h1>
                   </Reavel>
                   <Reavel>
                     <p className="whitespace-nowrap">High-Quality Products</p>
@@ -82,10 +90,11 @@ export default function Home({ to = '' }) {
                 </div>
               </div>
 
-
               <div className="flex flex-col justify-center items-center w-full sm:w-auto mt-4 sm:mt-0">
                 <Reavel>
-                  <h1 className="font-bold text-2xl sm:text-3xl tracking-wide">30,000+</h1>
+                  <h1 className="font-bold text-2xl sm:text-3xl tracking-wide">
+                    30,000+
+                  </h1>
                 </Reavel>
                 <Reavel>
                   <p>Happy Customers</p>
@@ -97,30 +106,35 @@ export default function Home({ to = '' }) {
           {/* الصورة */}
           <div
             className="lg:w-1/2 w-full h-screen"
-            style={{ width: '100vw', height: '85%', display: 'flex', justifyContent: 'center', alignItems: 'center', overflow: 'hidden' }}
-          >
+            style={{
+              width: "100vw",
+              height: "85%",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              overflow: "hidden",
+            }}>
             <img
               src="model.png"
               alt="Stylish couple wearing fashionable clothes"
-              style={{ width: '100vw', height: '85%', objectFit: 'cover' }}
+              style={{ width: "100vw", height: "85%", objectFit: "cover" }}
             />
           </div>
         </div>
       </div>
 
-
       <BrandsBar />
       <div ref={NewArrival}>
-        <HomeSliders text='NEW ARRIVALS' type='newarrival' />
+        <HomeSliders text="NEW ARRIVALS" type="newarrival" />
       </div>
       <div ref={TopSelling}>
-        <HomeSliders text='TOP SELLING' type='topselling' />
+        <HomeSliders text="TOP SELLING" type="topselling" />
       </div>
       <div ref={OnSale}>
-        <HomeSliders text='ON SALE' type='onsale' />
+        <HomeSliders text="ON SALE" type="onsale" />
       </div>
 
       <HappyCustomers />
     </>
-  )
+  );
 }
